c7d678e384747048cd148ce4a1a9fd8eab4b5259
Darko Andreev <darko.andreev@paysafe.com>
2020-03-22 22:46:38 +0200

    feat(ng-rating): JSDocs added

--- projects/ng-rating/src/lib/ng-rating.component.ts
+++ projects/ng-rating/src/lib/ng-rating.component.ts
@@ -26,6 +26,20 @@ export interface IRating {
 
 let UNIQUE_ID = 0;
 
+/**
+ * Rating components is a star based selection input.
+ * A star rating usually consists of images of stars that can be used to rate a particular item.
+ * A mouse user hovers over the stars and clicks one to select it.
+ * For example, if the user clicks on the third star from the left, the rating of the item is 3 of 5 stars.
+ * ```
+ * @example
+ * <ng-rating [input bindings]>
+ *  <ng-template ngRatingLabel>{{ this.ratingLabel }}</ng-template>
+ * </ng-rating>
+ * ```
+ *
+ * @export
+ */
 @Component({
   selector: 'ng-rating',
   templateUrl: './ng-rating.component.html',
@@ -34,44 +48,65 @@ let UNIQUE_ID = 0;
   providers: [NG_RATING_VALUE_ACCESSOR],
 })
 export class NgRatingComponent implements ControlValueAccessor {
-  @ContentChild(NgRatingLabelDirective) ratingLabelTemplate: NgRatingLabelDirective;
+  /** @hidden @internal */
+  @ContentChild(NgRatingLabelDirective) public ratingLabelTemplate: NgRatingLabelDirective;
 
   @HostBinding('class.ng-star-rating') get starRatingClass(): boolean {
     return true;
   }
+
+  /** A unique id for the rating input. If none is supplied, it will be auto-generated. */
   @HostBinding('attr.id')
   @Input()
-  id = `ng-star-rating-${UNIQUE_ID++}`;
+  public id = `ng-star-rating-${UNIQUE_ID++}`;
 
   /**
-   * Attached to the aria-label attribute of the host element. In most cases, aria-labelledby will
-   * take precedence so this may be omitted.
+   * Attached to the aria-label attribute of the host element.
+   * In most cases, aria-labelledby will take precedence so this may be omitted.
    */
   @HostBinding('attr.aria-label')
   @Input('aria-label')
-  ariaLabel = 'star';
+  public ariaLabel = 'star';
 
   /**
-   * Users can specify the `aria-labelledby` attribute which will be forwarded to the input element
+   * Users can specify the `aria-labelledby` attribute which will be forwarded to the input element.
    */
   @HostBinding('attr.aria-labelledby')
   @Input('aria-labelledby')
-  ariaLabelledby: string | undefined = 'Star rating';
+  public ariaLabelledby: string | undefined = 'Star rating';
 
+  /**
+   * Gets/sets the `rating` for the component.
+   * Determines selected rate items.
+   * @example
+   * ```html
+   * <ng-rating [rating]="5"></ng-rating>
+   * ```
+   * @memberOf NgRatingComponent
+   */
   @Input()
-  get rating(): number {
+  public get rating(): number {
     return this._rating;
   }
-  set rating(value: number) {
+  public set rating(value: number) {
     this._rating = coerceNumberProperty(value);
   }
   private _rating: number;
 
+  /**
+   * Gets/sets the `size` for the component.
+   * Sets max number of rate items.
+   * @example
+   * ```html
+   * <ng-rating [size]="5"></ng-rating>
+   * ```
+   * @memberOf NgRatingComponent
+   */
   @Input()
-  get size(): number {
+  public get size(): number {
     return this._size;
   }
-  set size(value: number) {
+  public set size(value: number) {
     if (value <= 0) {
       RATING_SIZE_ERROR();
     }
@@ -92,56 +127,146 @@ export class NgRatingComponent implements ControlValueAccessor {
   }
   private _size: number;
 
+  /**
+   * Gets/sets the `readonly` property.
+   * Determines if the rate component is readonly.
+   * By default it's **false**.
+   * @example
+   * ```html
+   * <ng-rating readonly></ng-rating>
+   * ```
+   * @memberOf NgRatingComponent
+   */
   @Input()
-  get readonly(): boolean {
+  public get readonly(): boolean {
     return this._readonly;
   }
-  set readonly(value: boolean) {
+  public set readonly(value: boolean) {
     this._readonly = coerceBooleanProperty(value);
   }
-  private _readonly: boolean;
+  private _readonly = false;
 
+  /**
+   * Gets/sets the `disabled` property.
+   * Whether the rate component is disabled.
+   * By default rate items are clickable (disabled=false).
+   * @example
+   * ```html
+   * <ng-rating disabled></ng-rating>
+   * ```
+   * @memberOf NgRatingComponent
+   */
   @Input()
-  get disabled(): boolean {
+  public get disabled(): boolean {
     return this._disabled;
   }
-  set disabled(value: boolean) {
+  public set disabled(value: boolean) {
     this._disabled = coerceBooleanProperty(value);
   }
-  private _disabled: boolean;
+  private _disabled = false;
 
+  /**
+   * Gets/sets the `showCancelIcon` property.
+   * Whether the cancel (clear) icon is visible.
+   * By default it's visible.
+   * @example
+   * ```html
+   * <ng-rating [showCancelIcon]="false"></ng-rating>
+   * ```
+   * @memberOf NgRatingComponent
+   */
   @Input()
-  get showCancelIcon(): boolean {
+  public get showCancelIcon(): boolean {
     return this._showCancelIcon;
   }
-  set showCancelIcon(value: boolean) {
+  public set showCancelIcon(value: boolean) {
     this._showCancelIcon = coerceBooleanProperty(value);
   }
   private _showCancelIcon = true;
 
-  @Input() icon: IconDefinition = faStar;
+  /**
+   * Gets/sets the `icon` for the rate item.
+   * By default it's **faStar** FontAwesome icon.
+   * @example
+   * ```html
+   * <ng-rating [icon]="faStar"></ng-rating>
+   * ```
+   * @memberOf NgRatingComponent
+   */
+  @Input() public icon: IconDefinition = faStar;
 
-  @Input() cancelIcon: IconDefinition = faBan;
+  /**
+   * Gets/sets the `cancelIcon` for the component.
+   * By default it uses **faBan** FontAwesome icon.
+   * @example
+   * ```html
+   * <ng-rating [cancelIcon]="faBan"></ng-rating>
+   * ```
+   * @memberOf NgRatingComponent
+   */
+  @Input() public cancelIcon: IconDefinition = faBan;
 
-  @Input() rateStyle: any;
+  /**
+   * Gets/sets the `rateStyle`. Custom style for rate item.
+   * By default it uses styles from the component.
+   * @example
+   * ```html
+   * <ng-rating [rateStyle]="{'font-size.px': fontSizeExp}"></ng-rating>
+   * ```
+   * @memberOf NgRatingComponent
+   */
+  @Input() public rateStyle: any;
 
-  @Input() cancelRateStyle: any;
+  /**
+   * Gets/sets the `cancelRateStyle`. Custom style for the cancel (clear) button.
+   * By default it uses styles from the component.
+   * @example
+   * ```html
+   * <ng-rating [cancelRateStyle]="{'font-size.px': fontSizeExp}"></ng-rating>
+   * ```
+   * @memberOf NgRatingComponent
+   */
+  @Input() public cancelRateStyle: any;
 
-  @Output() rateChange: EventEmitter<number> = new EventEmitter();
+  /**
+   * An event that is emitted after the rate item is clicked and set.
+   * Provides a number of clicked item - ex. 1,2,3, etc.
+   * @example
+   * ```html
+   * <ng-rating [rateChange]="change($event)"></ng-rating>
+   * ```
+   */
+  @Output() public rateChange: EventEmitter<number> = new EventEmitter();
 
-  @Output() rateCancel: EventEmitter<void> = new EventEmitter();
+  /**
+   * An event that is emitted after the rating is canceled (cleared).
+   * @example
+   * ```html
+   * <ng-rating [rateCancel]="cancel($event)"></ng-rating>
+   * ```
+   */
+  @Output() public rateCancel: EventEmitter<void> = new EventEmitter();
 
   private _selectedIndex = -1;
 
+  /** @hidden @internal */
   public ratings: IRating[];
 
-  hoveredItem(index: number): void {
+  // Function to call when the rating changes.
+  private _controlValueAccessorChangeFn: (value: any) => void = () => {};
+
+  // Function to call when the input is touched (when a star is clicked).
+  private onTouched: () => any = () => {};
+
+  /** @hidden @internal */
+  public hoveredItem(index: number): void {
     if (!this.readonly) {
       this.ratings.forEach((item, i) => (item.hovered = index >= i));
     }
   }
 
-  mouseLeave(): void {
+  /** @hidden @internal */
+  public mouseLeave(): void {
     if (!this.readonly) {
       this.ratings.forEach((item, index) => {
         item.clicked = false;
@@ -150,7 +275,8 @@ export class NgRatingComponent implements ControlValueAccessor {
     }
   }
 
-  itemClick(item: IRating, index: number): void {
+  /** @hidden @internal */
+  public itemClick(item: IRating, index: number): void {
     if (!this.readonly) {
       this._selectedIndex = index;
       item.clicked = true;
@@ -160,7 +286,8 @@ export class NgRatingComponent implements ControlValueAccessor {
     }
   }
 
-  cancel(): void {
+  /** @hidden @internal */
+  public cancel(): void {
     this._selectedIndex = -1;
     this.ratings.forEach((item) => {
       item.hovered = false;
@@ -169,13 +296,8 @@ export class NgRatingComponent implements ControlValueAccessor {
     this.rateCancel.emit();
   }
 
-  // Function to call when the rating changes.
-  _controlValueAccessorChangeFn: (value: any) => void = () => {};
-
-  // Function to call when the input is touched (when a star is clicked).
-  onTouched: () => any = () => {};
-
-  writeValue(rating: number): void {
+  /** @hidden @internal */
+  public writeValue(rating: number): void {
     this._controlValueAccessorChangeFn(rating);
 
     this.rating = rating;
@@ -183,15 +305,18 @@ export class NgRatingComponent implements ControlValueAccessor {
     this.ratings.forEach((item, i) => (item.hovered = rating - 1 >= i));
   }
 
-  registerOnChange(fn: (value: any) => void): void {
+  /** @hidden @internal */
+  public registerOnChange(fn: (value: any) => void): void {
     this._controlValueAccessorChangeFn = fn;
   }
 
-  registerOnTouched(fn: any): void {
+  /** @hidden @internal */
+  public registerOnTouched(fn: any): void {
     this.onTouched = fn;
   }
 
-  setDisabledState(value: boolean): void {
+  /** @hidden @internal */
+  public setDisabledState(value: boolean): void {
     this.disabled = value;
   }
 }
