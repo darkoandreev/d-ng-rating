b90eee72cc6706e14718bc63bc322dea20061062
Darko Andreev <darko.andreev@paysafe.com>
2020-04-03 00:35:39 +0300

    Auto-generated commit

---
+++ ng-rating/esm5/lib/ng-rating-label.directive.js
@@ -0,0 +1,24 @@
+import { __decorate } from "tslib";
+import { Directive, HostBinding } from '@angular/core';
+var NgRatingLabelDirective = /** @class */ (function () {
+    function NgRatingLabelDirective() {
+    }
+    Object.defineProperty(NgRatingLabelDirective.prototype, "ngRatingLabel", {
+        get: function () {
+            return true;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    __decorate([
+        HostBinding('class.ng-rating-label')
+    ], NgRatingLabelDirective.prototype, "ngRatingLabel", null);
+    NgRatingLabelDirective = __decorate([
+        Directive({
+            selector: '[ngRatingLabel], ng-rating-label',
+        })
+    ], NgRatingLabelDirective);
+    return NgRatingLabelDirective;
+}());
+export { NgRatingLabelDirective };
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibmctcmF0aW5nLWxhYmVsLmRpcmVjdGl2ZS5qcyIsInNvdXJjZVJvb3QiOiJuZzovL25nLXJhdGluZy8iLCJzb3VyY2VzIjpbImxpYi9uZy1yYXRpbmctbGFiZWwuZGlyZWN0aXZlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSxPQUFPLEVBQUUsU0FBUyxFQUFlLFdBQVcsRUFBRSxNQUFNLGVBQWUsQ0FBQztBQUtwRTtJQUFBO0lBSUEsQ0FBQztJQUh1QyxzQkFBSSxpREFBYTthQUFqQjtZQUNwQyxPQUFPLElBQUksQ0FBQztRQUNkLENBQUM7OztPQUFBO0lBRnFDO1FBQXJDLFdBQVcsQ0FBQyx1QkFBdUIsQ0FBQzsrREFFcEM7SUFIVSxzQkFBc0I7UUFIbEMsU0FBUyxDQUFDO1lBQ1QsUUFBUSxFQUFFLGtDQUFrQztTQUM3QyxDQUFDO09BQ1csc0JBQXNCLENBSWxDO0lBQUQsNkJBQUM7Q0FBQSxBQUpELElBSUM7U0FKWSxzQkFBc0IiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBEaXJlY3RpdmUsIFRlbXBsYXRlUmVmLCBIb3N0QmluZGluZyB9IGZyb20gJ0Bhbmd1bGFyL2NvcmUnO1xuXG5ARGlyZWN0aXZlKHtcbiAgc2VsZWN0b3I6ICdbbmdSYXRpbmdMYWJlbF0sIG5nLXJhdGluZy1sYWJlbCcsXG59KVxuZXhwb3J0IGNsYXNzIE5nUmF0aW5nTGFiZWxEaXJlY3RpdmUge1xuICBASG9zdEJpbmRpbmcoJ2NsYXNzLm5nLXJhdGluZy1sYWJlbCcpIGdldCBuZ1JhdGluZ0xhYmVsKCk6IGJvb2xlYW4ge1xuICAgIHJldHVybiB0cnVlO1xuICB9XG59XG4iXX0=
---
+++ ng-rating/lib/ng-rating.error.d.ts
@@ -0,0 +1,2 @@
+export declare const RATING_SIZE_ERROR: () => never;
+export declare const RATE_SET_ERROR: () => never;
