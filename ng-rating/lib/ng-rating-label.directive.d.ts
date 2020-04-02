c901529026fe4d4f349fc129d5ae66ac41587f3e
Darko Andreev <darko.andreev@paysafe.com>
2020-04-03 00:35:49 +0300

    Auto-generated commit

--- ng-rating/lib/ng-rating-label.directive.d.ts
+++
@@ -1,8 +0,0 @@
-import * as ɵngcc0 from '@angular/core';
-export declare class NgRatingLabelDirective {
-    get ngRatingLabel(): boolean;
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<NgRatingLabelDirective, never>;
-    static ɵdir: ɵngcc0.ɵɵDirectiveDefWithMeta<NgRatingLabelDirective, "[ngRatingLabel], ng-rating-label", never, {}, {}, never>;
-}
-
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibmctcmF0aW5nLWxhYmVsLmRpcmVjdGl2ZS5kLnRzIiwic291cmNlcyI6WyJuZy1yYXRpbmctbGFiZWwuZGlyZWN0aXZlLmQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBOzs7O0FBRUEiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVjbGFyZSBjbGFzcyBOZ1JhdGluZ0xhYmVsRGlyZWN0aXZlIHtcbiAgICBnZXQgbmdSYXRpbmdMYWJlbCgpOiBib29sZWFuO1xufVxuIl19
