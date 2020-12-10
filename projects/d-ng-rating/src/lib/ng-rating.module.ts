94356efa637a1d06ecec242ceea758e87ea45445
Darko Andreev <andreev.darko@gmail.com>
2020-12-10 21:59:12 +0100

    feat/angular-10-upgrade
    * Update flex-layout package
    
    * tslib updated
    
    * angular/core and angular/cli updated
    
    * angular/cdk and angular/material updated
    
    * rxjs updated
    
    * peer dependencies updated
    
    Co-authored-by: Darko Andreev <darko.andreev@paysafe.com>

--- projects/d-ng-rating/src/lib/ng-rating.module.ts
+++ projects/d-ng-rating/src/lib/ng-rating.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { NgRatingComponent } from './ng-rating.component';
 import { CommonModule } from '@angular/common';
-import { HammerModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms';
 import { NgRatingLabelDirective } from './ng-rating-label.directive';
 
 @NgModule({
   declarations: [NgRatingComponent, NgRatingLabelDirective],
-  imports: [CommonModule, FormsModule, HammerModule, FontAwesomeModule],
+  imports: [CommonModule, FormsModule, FontAwesomeModule],
   exports: [NgRatingComponent, NgRatingLabelDirective],
 })
 export class NgRatingModule {}
